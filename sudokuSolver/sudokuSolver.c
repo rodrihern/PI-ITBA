8c341cddf553b92b846b9553828247ff05fa0bbb
rodrihern <rohernandez003@gmail.com>
2024-06-03 13:50:28 +0000

    sudokuSolver en O(n!)

--- sudokuSolver/sudokuSolver.c
+++ sudokuSolver/sudokuSolver.c
@@ -3,7 +3,8 @@
 #include <assert.h>
 
 /*
-    
+        * DISCLAIMER *
+
     NO ES EFICIENTE PARA NADA
     LO HICE PARA PRACTICAR RECURSIVIDAD Y PORQUE TENIA GANAS
     CUANDO APRENDA BACKTRACKING LO MEJORARE
@@ -41,20 +42,18 @@ int main(void) {
     // sudoku de respuesta
     tSudoku solution;
 
-    // faltan 14 numeros, le cuesta pero lo logra
     tSudoku sudoku = {
-        {5,3,4,6,7,8,9,0,2}, 
-        {0,7,0,1,9,5,3,4,8}, 
-        {0,9,8,3,4,0,5,6,7}, 
-        {8,5,9,7,6,1,4,0,3}, 
-        {4,0,6,0,5,3,7,9,1}, 
-        {7,1,3,9,2,4,8,0,6}, 
-        {9,6,1,0,3,7,2,8,0}, 
-        {2,0,7,0,1,0,0,3,5}, 
-        {3,4,5,2,8,6,1,7,9} 
-    };
+        {3,8,1,9,7,6,5,4,2}, 
+        {2,4,7,0,3,8,1,0,6},
+        {5,0,9,2,1,0,8,7,3},
+        {6,7,4,8,5,2,3,0,9},
+        {1,3,5,7,0,9,6,2,8},
+        {9,0,8,1,6,3,7,5,4},
+        {0,1,0,6,8,5,9,0,7},
+        {7,0,6,3,0,1,4,8,5},
+        {8,5,3,4,9,0,2,6,1}
 
-    
+    };
 
     puts("sudoku a resolver:");
     printSudoku(sudoku);
