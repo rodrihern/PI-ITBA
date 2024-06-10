6a3c04642ce1e38fba2f31e94a59ab076875f12e
rodrihern <rohernandez003@gmail.com>
2024-06-10 09:42:15 -0300

    termine addressBookADT

--- Talleres/addressBookADT/addressBookADT.c
+++ Talleres/addressBookADT/addressBookADT.c
@@ -1,60 +1,155 @@
 #include "addressBookADT.h"
+#include <string.h>
+#include <strings.h>
+#include <stdlib.h>
 
-typedef struct contactList{
+typedef struct contactNode{
     tContact info;
-    struct contactList * tail;
-} contactList;
+    struct contactNode * tail;
+} contactNode;
 
-typedef struct groupList{
+typedef contactNode * contactList;
+
+typedef struct groupNode{
     char * name;
-    contactList * first;
-    struct groupList * tail;
-} groupList;
+    contactList firstContact;
+    struct groupNode * tail;
+} groupNode;
+
+typedef groupNode * groupList;
 
 typedef struct addressBookCDT {
-    groupList * first;
-    groupList * groupIterator;
-    contactList * contactIterator;
-
+    groupList firstGroup;
+    groupList nextGroup;
+    contactList nextContact;
 } addressBookCDT;
 
 
 /* Crea un nuevo TAD vacÃ­o */
-addressBookADT newAddressBook();
+addressBookADT newAddressBook() {
+    return calloc(1, sizeof(addressBookCDT));
+}
 
-/* Almacena un nuevo grupo de contactos. Si el grupo existe, no hace nada.
-** No diferencia entre minÃºsculas y mayÃºsculas, el grupo "abc" es el mismo que "ABC",
-** "Abc", etc. pero distinto que " abc" o "abc "
-** Almacena una copia del nombre, que no tiene un lÃ­mite para su longitud
-*/
-int addGroup(addressBookADT, const char * groupName);
+static char * cpyStr(const char * s) {
+    return strcpy(malloc(strlen(s) + 1), s);
+}
 
-/* Si existe un grupo de nombre groupName, agrega el contacto contact a la agenda
-** Si el grupo no existe, no hace nada
-** Si el contact ya estaba relacionado al grupo (habÃ­a uno con el mismo nombre),
-** no lo agrega
-*/
-int addContact(addressBookADT, const char * groupName, tContact contact);
 
-/*
-** Permite iterar por todos los contactos de la agenda
-** QUE PERTENEZCAN AL GRUPO INDICADO por parÃ¡metro en toBegin
-** El orden es alfabÃ©tico por nombre del contacto.
-*/
-void toBeginForGroup(addressBookADT, const char * groupName);
+// inserta ordenado un grupo y deja en added un 1 si lo agrego
+static groupList insertGroup(groupList list, const char * name, int * added) {
+    int c;
+    if(list == NULL || (c = strcasecmp(list->name, name)) > 0) {
+        // inserto
+        *added = 1;
+        groupList new = malloc(sizeof(groupNode));
+        new->name = cpyStr(name);
+        new->firstContact = NULL;
+        new->tail = list;
+        return new;
+    }
+    if(c < 0) {
+        // inserto en la cola
+        list->tail = insertGroup(list->tail, name, added);
+    }
+    //retorno porque el elemento ya existia
+    return list;
+}
 
-int hasNextForGroup(addressBookADT);
+int addGroup(addressBookADT ab, const char * groupName) {
+    int added = 0;
+    ab->firstGroup = insertGroup(ab->firstGroup, groupName, &added);
+    return added;
+}
 
-/* Retorna un contacto, aborta si no hay siguiente */
-tContact nextForGroup(addressBookADT);
 
-/*
-** Permite iterar por todos los grupos de la agenda
-** El orden es alfabÃ©tico por nombre del grupo.
-*/
-void toBegin(addressBookADT);
+// retorna un puntero al grupo si el grupo existe, NULL si no
+static groupList findGroup(groupList list, const char * name) {
+    groupList ans = NULL;
+    while(!ans && list) {
+        if(strcasecmp(list->name, name) == 0) {
+            ans = list;
+        }
+        list = list->tail;
+    }
+    return ans; 
+}
 
-int hasNext(addressBookADT);
+// inserta un contacto ordenado y deja en added un 1 si lo agrego
+static contactList insertContact(contactList list, tContact contact, int * added) {
+    int c;
+    if(list == NULL || (c = strcasecmp(list->info.name, contact.name)) > 0) {
+        //inserto
+        *added = 1;
+        contactList new = malloc(sizeof(contactNode));
+        new->info = contact;
+        new->tail = list;
+        return new;
+    }
+    if(c < 0) {
+        // inserto en la cola
+        list->tail = insertContact(list->tail, contact, added);
+    }
+    // ya existia el contacto
+    return list;
+}
 
-/* Retorna una copia del grupo, aborta si no hay siguiente */
-char * next(addressBookADT);
+
+
+int addContact(addressBookADT ab, const char * groupName, tContact contact) {
+    groupList group = findGroup(ab->firstGroup, groupName);
+    if(group == NULL) {
+        return 0;
+    }
+    int added = 0;
+    group->firstContact = insertContact(group->firstContact, contact, &added);
+    return added;
+}
+
+
+
+void toBeginForGroup(addressBookADT ab, const char * groupName) {
+    groupList group = findGroup(ab->firstGroup, groupName);
+    if(group == NULL) {
+        return;
+    }
+    ab->nextContact = group->firstContact;
+}
+
+
+
+int hasNextForGroup(addressBookADT ab) {
+    return ab->nextContact != NULL;
+}
+
+
+tContact nextForGroup(addressBookADT ab) {
+    if(!hasNextForGroup(ab)) {
+        exit(1);
+    }
+    tContact ans = ab->nextContact->info;
+    ab->nextContact = ab->nextContact->tail;
+    return ans;
+}
+
+
+
+void toBegin(addressBookADT ab) {
+    ab->nextGroup = ab->firstGroup;
+}
+
+
+
+int hasNext(addressBookADT ab) {
+    return ab->nextGroup != NULL;
+}
+
+
+
+char * next(addressBookADT ab) {
+    if(!hasNext(ab)) {
+        exit(1);
+    }
+    char * ans = cpyStr(ab->nextGroup->name) ;
+    ab->nextGroup = ab->nextGroup->tail;
+    return cpyStr(ans);
+}
